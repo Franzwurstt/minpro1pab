5ea256c382662d9688b4c1313ca76826f11303a7
Franzwurstt <prassmukti@gmail.com>
2026-02-26 14:44:46 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Katalog Samsung',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.red),
       home: const HomePage(),
     );
   }
