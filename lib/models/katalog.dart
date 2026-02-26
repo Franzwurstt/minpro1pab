89063b745e07473fd9ac165c9293c27b83a3dbc8
Franzwurstt <prassmukti@gmail.com>
2026-02-26 13:49:29 +0800

    Update katalog.dart

--- lib/models/katalog.dart
+++ lib/models/katalog.dart
@@ -3,11 +3,13 @@ class Katalog {
   String harga;
   String tahun;
   String gambar;
+  String spesifikasi;
 
   Katalog({
     required this.nama,
     required this.harga,
     required this.tahun,
     required this.gambar,
+    required this.spesifikasi,
   });
 }
